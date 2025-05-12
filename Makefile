f9ec69b5420cf13d66251110ac650d319aa41249
Jeff Candy <candy@jaamweb.com>
2025-05-12 10:02:24 -0700

    Proper removal of source files

--- Makefile
+++ Makefile
@@ -2,8 +2,7 @@ all:
 	cd src ; make html
 
 sync:
-#	rsync -a --exclude-from='exclude.txt' src/_build/html/ doc/
-	rsync -a src/_build/html/ doc/
+	rsync --delete -a src/_build/html/ doc/
 
 clean:
 	cd src ; make clean
--- src/conf.py
+++ src/conf.py
@@ -38,6 +38,7 @@ html_favicon         = 'images/logo/favicon.ico'
 html_show_sphinx     = True
 html_show_copyright  = True
 html_show_sourcelink = False
+html_copy_source     = False
 html_last_updated_fmt = '%b %d, %Y'
 
 html_theme_options = {'analytics_id': '',
